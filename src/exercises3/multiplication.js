23c9c553a37fa9f07040a31f665c20ff2fc9bed9
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-23 08:37:06 +0200

    multiplication

---
+++ src/exercises3/multiplication.js
@@ -0,0 +1,7 @@
+// Refactor the different functions below by improving their structure and code quality.
+// function mult(x,y){
+//     return x*y;
+//   }
+
+const multplication = (x, y) => x * y;
+
