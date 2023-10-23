64c7fbfde8f5bf65189a5264491b5596fa70587b
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-23 12:25:55 +0200

    nested array

---
+++ src/exercises4/nesting-array.js
@@ -0,0 +1,8 @@
+/**
+Write a function(s) that checks the level of nesting of an array. Then, 
+use that function to flatten the array into a single-dimensional array.
+*/
+
+function NestingArray() {
+  return 0;
+}
