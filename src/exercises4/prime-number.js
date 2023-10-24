ed42fcb2a290e4527378228532d1c9c8cc704726
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-24 10:45:11 +0200

    refactor prime number

--- src/exercises4/prime-number.js
+++ src/exercises4/prime-number.js
@@ -1,10 +1,9 @@
 //Write a JavaScript function to check if a given number is prime or not
 
 function primeNumber(number) {
-  let divisor = 2;
-  while (number > divisor) {
+  for (let divisor = 2; number > divisor; divisor++) {
     if (number % divisor === 0) return false;
-    divisor++;
   }
   return true;
 }
+console.log(primeNumber(68));
