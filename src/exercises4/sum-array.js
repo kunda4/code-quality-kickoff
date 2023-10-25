995dfddb9a79bdfbdbc8b58aaf94ef0737fcda90
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-25 12:06:47 +0200

    refactor sum array

--- src/exercises4/sum-array.js
+++ src/exercises4/sum-array.js
@@ -1,11 +1,9 @@
 //Implement a JavaScript function to find the sum of all elements in an array recursively.
 
 function sumArray(array, index = 0) {
-  if (index === array.length) return 0;
+  if (array.length === index) return 0;
 
   let sum = sumArray(array, index + 1);
 
   return sum + array[index];
 }
-
-console.log(sumArray([1, 2, 3, 4, 5, 6]));
